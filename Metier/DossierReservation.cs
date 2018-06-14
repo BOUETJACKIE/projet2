3438e052b176f88310582e06b61e56c492c99039
Cyrille <Cyl-Dev@hotmail.com>
2018-06-14 09:55:41 +0200

    Renommage du projet

---
+++ Metier/DossierReservation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExempleUtilisationBoVoyageFramework.Metier
+{
+    public class DossierReservation
+    {
+        int NumeroUnique { get; set; }
+
+    }
+}
