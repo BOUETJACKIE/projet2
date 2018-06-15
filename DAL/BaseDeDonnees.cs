0265735707a7f5784b35ae33e018568f4ca8123f
bouetjackie <jackiebouet@hotmail.fr>
2018-06-15 14:54:59 +0200

    modif gestion voyage

--- DAL/BaseDeDonnees.cs
+++ DAL/BaseDeDonnees.cs
@@ -20,8 +20,8 @@
         public DbSet<Participant> Participants { get; set; }
         public DbSet<Agence> Agences { get; set; }
         public DbSet<Assurance> Assurances { get; set; }
-        /*public DbSet<Client> Clients { get; set; }
-       */ public DbSet<Destination> Destinations { get; set; }
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Destination> Destinations { get; set; }
        /* public DbSet<DossierReservation> DossierReservations { get; set; }
         */
         public DbSet<Voyage> Voyages { get; set; }
--- UI/ModuleDeGestionVoyage.cs
+++ UI/ModuleDeGestionVoyage.cs
@@ -75,12 +75,42 @@ namespace BoVoyageEtape2.UI
 
         }
 
-        private void Nouveau()
+        public void AjouterVoyage()
         {
-            ConsoleHelper.AfficherEntete("Nouveau");
+            
+                ConsoleHelper.AfficherEntete("Nouveau Voyage");
 
-            Console.WriteLine("TO DO");
+                var voyage = new Voyage();
+                voyage.DateAller = ConsoleSaisie.SaisirDateObligatoire("Date de départ: ");
+                voyage.DateRetour = ConsoleSaisie.SaisirDateObligatoire("Date de retour: ");
+                voyage.PlacesDispo = ConsoleSaisie.SaisirEntierObligatoire("Nombre de places disponibles: ");
+                voyage.TarifToutCompris = ConsoleSaisie.SaisirDecimalObligatoire(" tarif du voyage tout inclus:");
+
+                //voyage.Destination = this.ServiceDestination.GetDestination(voyage.Destination);
+
+
+                serviceVoyage.AjouterVoyage(voyage);
+           
+
+
+                if (voyage.DateRetour < voyage.DateAller)
+                {
+                    throw new MetierException("ATTENTION!! La date de retour ne doit pas être antérieure à la date de départ ");
+                }
+
+                if (voyage.PlacesDispo == 0)
+                {
+                    throw new MetierException("Voyage complet!!");
+                }
+
+                // Ecriture en base
+                using (var dal = new BaseDeDonnees())
+                {
+                    dal.Voyages.Add(voyage);
+                    dal.SaveChanges();
+                }
+            }
+
         }
-
     }
 }
